b2196823ec38667ed819b52a77e7ff6e7558108e
Eduardo <el_alienx@hotmail.com>
2024-04-22 23:09:22 +0200

    refactor client

--- client.js
+++ client.js
@@ -1,17 +1,20 @@
-const eventSource = new EventSource('http://localhost:8000')
+// Properties
+const port = 8000;
+const eventSource = new EventSource(`http://localhost:${port}`);
 
-function updateMessage (message) {
-  const list = document.getElementById('messages')
-  const item = document.createElement('p')
-  item.textContent = message
-  list.appendChild(item)
+function updateMessage(message) {
+  const list = document.getElementById("messages");
+  const item = document.createElement("p");
+
+  item.textContent = message;
+  list.appendChild(item);
 }
 
 eventSource.onmessage = function (event) {
-  updateMessage(event.data)
-}
+  updateMessage(event.data);
+};
 
 eventSource.onerror = function () {
-  updateMessage('Server closed connection')
-  eventSource.close()
-}
+  updateMessage("Server closed connection ❌");
+  eventSource.close();
+};
